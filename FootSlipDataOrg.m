2c998471ee2c278e9a237b0486dc1b47f162f1c9
jjv432 <jackvranicar43@gmail.com>
2025-02-04 14:28:42 -0500

    Difficulty generating maxima for each point

--- FootSlipDataOrg.m
+++ FootSlipDataOrg.m
@@ -3,6 +3,10 @@ clc; clearvars -except Data; close all; format compact;
 addpath("SlippingFunctions");
 addpath("TrackerFiles");
 
+%{
+
+Need to re-write this so that 
+%}
 %% Sorting file Names
 FileNames = string(ls("TrackerFiles"));
 FullFileNames = FileNames(3:end, :);
@@ -52,6 +56,7 @@ for i = 1:numel(FullFileNames)
   
     %\cite{https://www.mathworks.com/matlabcentral/answers/152301-find-closest-value-in-array#comment_2806253}
     [~, t_vals_idx] = min(abs(Data.(AbrevFileNames(i)).t - t_vals));
+    Data.(AbrevFileNames(i)).t_vals = t_vals;
   
 
     figure();
@@ -71,6 +76,48 @@ for i = 1:numel(FullFileNames)
         plot(curT, curX);
         
     end
+
+end
+
+%% Taking statistical data from each stride
+% Could combine this with the previous step to help with speed
+
+for i = 1:numel(FullFileNames)
+    cur_fields = fieldnames(Data.(AbrevFileNames(i)));
+    cur_num_strides = sum(contains(cur_fields, "Stride"));
+    
+    x_tot = [];
+    for k = 1:cur_num_strides
+        cur_x = Data.(AbrevFileNames(i)).(strcat("Stride_", num2str(k))).x;
+        % This works because we're assuming dt is constant
+        x_tot = [x_tot; cur_x'];
+
+    end
+
+    % Taking the mean and std dev by column (each dt)
+    mean_x = mean(x_tot, 1);
+    std_x = std(x_tot, 0, 1);
+    [~,max_index]= max(x_tot,[], 1);
+    helper_column = 1:numel(max_index);
+    max_x_vals = x_tot(helper_column, max_index);
+
+    Data.(AbrevFileNames(i)).mean_x = mean_x;
+    Data.(AbrevFileNames(i)).std_x = std_x;
+
+    
+    Data.(AbrevFileNames(i)).mean_x = mean_x;
+    num_dts = numel(mean_x);
+    cur_tvals = Data.(AbrevFileNames(i)).t_vals;
+    cur_x_axis = 1:num_dts;
+    figure();
+    hold on
+    % \cite{https://www.mathworks.com/matlabcentral/answers/1928100-create-plot-with-shaded-standard-deviation-but#answer_1192320}
+    % !!!! need to fix this so that the x values are real !!!!!!
+
+    fill([cur_x_axis, flip(cur_x_axis)], [mean_x + std_x, flip(mean_x - std_x)], [0.8 0.8 0.8]);
+    plot(cur_x_axis, mean_x, '--k');
+    plot(cur_x_axis, max_x_vals, '--k');
+    hold off
 
 end
 
